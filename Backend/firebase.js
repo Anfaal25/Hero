fdbe9dde7a94d0f056ce9666dea5424e98ae6f98
Charbel Maroun <charbel.maroun@ucalgary.ca>
2024-11-09 15:44:41 -0700

    Added Database

---
+++ Backend/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBAXigvl3aBqGBMgD7ltyqhOsMNT-WuZFo",
+    authDomain: "myapp-c6065.firebaseapp.com",
+    projectId: "myapp-c6065",
+    storageBucket: "myapp-c6065.firebasestorage.app",
+    messagingSenderId: "597743777633",
+    appId: "1:597743777633:web:84e54fad768339dadf7720"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
