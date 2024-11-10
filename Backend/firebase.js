75b48e46d03d3ca7cb67f0960630e5df9a5b8123
Charbel Maroun <charbel.maroun@ucalgary.ca>
2024-11-09 21:16:14 -0700

    g

--- Backend/firebase.js
+++ Backend/firebase.js
@@ -1,22 +1,16 @@
-const { initializeApp } = require("firebase/app");
-const { getFirestore, collection, addDoc, getDocs } = require("firebase/firestore");
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: "myapp-c6065.firebaseapp.com",
-    projectId: "myapp-c6065",
-    storageBucket: "myapp-c6065.firebasestorage.app",
-    messagingSenderId: "597743777633",
-    appId: "1:597743777633:web:84e54fad768339dadf7720"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
-module.exports = { db, collection, addDoc, getDocs };
+export { db };
